be3ff21413de9755117919e9cdde46fd13ae4c83
Alex <adkipnis@mailbox.org>
2020-07-31 20:18:07 +0200

    changed pipeline structure

---
+++ 03 Creating & Testing Masks /01-FS_register_Glasser_annots.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Register Glasser surface atlas from standard space to native space (fsaverage -> fsnative)
+
+# Prerequisite: fmriprep's freesurfer output is in the derivatives directory and the Glasser surface atlas (rh/lh.HCP-MMP1.annot) in the .../fsaverage/label/ directory
+
+# Options: -p - plot atlas on fsaverage surface
+#          -s - plot atlas on native surface of sub-01
+#          -ps - first -p then -s
+
+cd $SUBJECTS_DIR     
+
+# Register
+for (( sub=1; sub <= 5; sub=sub+1 )); do
+    mkdir -p sub-0$sub/label_glasser/
+    for hemi in lh rh; do
+        mri_surf2surf \
+            --srcsubject fsaverage \
+            --trgsubject sub-0$sub \
+            --hemi $hemi \
+            --sval-annot fsaverage/label/$hemi.HCP-MMP1.annot \
+            --tval  sub-0$sub/label_glasser/$hemi.HCP-MMP1.annot
+    done
+done
+
+
+# Optionally plot annots on surface
+if  [[ $1 = "-p" ]]; then
+	freeview -f fsaverage/surf/lh.white:annot=fsaverage/label/lh.HCP-MMP1.annot
+fi
+
+if  [[ $1 = "-s" ]]; then
+	freeview -f sub-01/surf/lh.white:annot=sub-01/label_glasser/lh.HCP-MMP1.annot
+fi
+
+if  [[ $1 = "-ps" ]]; then
+	freeview -f fsaverage/surf/lh.white:annot=fsaverage/label/lh.HCP-MMP1.annot
+	freeview -f sub-01/surf/lh.white:annot=sub-01/label_glasser/lh.HCP-MMP1.annot
+fi
+
+
+
---
+++ 03 Creating & Testing Masks /ANTs_Glasser_T1w.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Apply ANTs registration of Glasser Atlas to individual T1w space 
+      
+data_path="/home/alex/Datasets/ds001246/derivatives/"
+input_path="/home/alex/Datasets/templateflow/tpl-Glasser/MMP_in_MNI_corr.nii.gz"
+
+for (( sub=1; sub <= 5; sub=sub+1 )); do
+	
+	anat_path=$data_path"fmriprep/sub-0"$sub"/anat/"
+	ref_path=$anat_path"sub-0"$sub"_desc-brain_mask.nii.gz"
+	transform_path=$anat_path"sub-0"$sub"_from-MNI152NLin2009cAsym_to-T1w_mode-image_xfm.h5"
+	output_path=$data_path"ROI_masks/T1w/sub-0"$sub"_Mask_T1w_Glasser.nii.gz"
+
+
+
+	antsApplyTransforms \
+		-i $input_path \
+		-r $ref_path \
+		-t $transform_path \
+		-n NearestNeighbor -o \
+		-o $output_path \
+		-v 1
+done
+
