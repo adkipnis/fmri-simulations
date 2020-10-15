d25d46a44c254111730edd50d4eff1550f0979a3
Alex <adkipnis@mailbox.org>
2020-10-15 22:47:16 +0200

    indent get_runs.m

--- 04 Simulation Scripts/Simulation_Utils/get_runs.m
+++ 04 Simulation Scripts/Simulation_Utils/get_runs.m
@@ -1,7 +1,7 @@
 function Dirs = get_runs(Dirs, s)
-ses_dir = string(fullfile(Dirs.sub_dir, Dirs.sesh_list(s)));            
-ses_dir_contents = dir(ses_dir);
-ses_dir_fnames = extractfield(ses_dir_contents, 'name');
-run_filt = logical(contains(ses_dir_fnames, 'run'));
-Dirs.n_runs = sum(run_filt);
+    ses_dir = string(fullfile(Dirs.sub_dir, Dirs.sesh_list(s)));            
+    ses_dir_contents = dir(ses_dir);
+    ses_dir_fnames = extractfield(ses_dir_contents, 'name');
+    run_filt = logical(contains(ses_dir_fnames, 'run'));
+    Dirs.n_runs = sum(run_filt);
 end
