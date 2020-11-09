c69197557bbca27961506af2a8aeaa325486fe9a
Alex <adkipnis@mailbox.org>
2020-11-09 17:02:33 +0100

    adapted permutation vector creation

--- 04 Simulation Scripts/Simulation_Utils/get_permutation_vectors.m
+++ 04 Simulation Scripts/Simulation_Utils/get_permutation_vectors.m
@@ -1,7 +1,7 @@
 function p_mat = get_permutation_vectors(Opts)
-    perm_idx = zeros(Opts.n_permutations, Opts.n_timepoints-1);
+    perm_idx = zeros(Opts.n_permutations, Opts.n_timepoints-Opts.ar_n);
     for i = 1 : Opts.n_permutations
-        perm_idx(i,:) = randperm(Opts.n_timepoints-1)+1;
+        perm_idx(i,:) = randperm(Opts.n_timepoints-Opts.ar_n)+Opts.ar_n;
     end
-    p_mat = horzcat(ones(Opts.n_permutations, 1), perm_idx);
+    p_mat = horzcat(repmat([1:Opts.ar_n], Opts.n_permutations, 1), perm_idx);
 end
